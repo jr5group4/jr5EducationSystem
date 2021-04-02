d04e106819cfb94bbef99bcc3bba99440fbfc4f1
poorn <poorn@LAPTOP-NCVJD8DF>
2021-04-02 15:11:52 +0530

    by Poornaprasad

---
+++ education-system/src/main/java/com/cg/educationsystem/dao/ITrainerRepository.java
@@ -0,0 +1,11 @@
+package com.cg.educationsystem.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.educationsystem.entity.Trainer;
+
+@Repository
+public interface ITrainerRepository extends JpaRepository<Trainer,Integer>{
+
+}
