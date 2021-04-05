db62da1ecf4994308bcaaa79e6878de2c70a8494
poorn <poorn@LAPTOP-NCVJD8DF>
2021-04-05 17:14:22 +0530

    commit by Poornaprasad

--- education-system/src/main/java/com/cg/educationsystem/controller/LoginController.java
+++ education-system/src/main/java/com/cg/educationsystem/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.cg.educationsystem.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class LoginController {
 	
 	@Autowired
 	LoginService loginService;
-	
+	@PostMapping("/validate")
 	public ResponseEntity<String> loginValidation(@RequestBody Login login){
 		String str=loginService.loginValidation(login);
 		return new ResponseEntity<String>(str,HttpStatus.OK);
