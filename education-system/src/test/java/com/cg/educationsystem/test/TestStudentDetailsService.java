7d251a149866d941e84136c8123ba476224eb0b2
poorn <poorn@LAPTOP-NCVJD8DF>
2021-04-06 17:25:57 +0530

    commit by Poornaprasad

--- education-system/src/test/java/com/cg/educationsystem/test/TestStudentDetailsService.java
+++ education-system/src/test/java/com/cg/educationsystem/test/TestStudentDetailsService.java
@@ -1,75 +1,51 @@
 package com.cg.educationsystem.test;
 
-
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.cg.educationsystem.dao.IStudentDetailsRepository;
-import com.cg.educationsystem.entity.Course;
-import com.cg.educationsystem.entity.Payment;
 import com.cg.educationsystem.entity.StudentDetails;
 import com.cg.educationsystem.service.StudentDetailsService;
-
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestStudentDetailsService {
 	@InjectMocks
-	StudentDetailsService studentDetailsService;
+	StudentDetailsService studentService;
 	
 	@Mock
-	IStudentDetailsRepository studentDetailsDao;
+	IStudentDetailsRepository studentDao;
 	
-	@Before
-	public void init() {
-		System.out.println("** before method **");
-		//MockitoAnnotations.initMocks(this);
+	@Test
+	public void testGetAllStudents() {
+		List<StudentDetails> studentList=new ArrayList<StudentDetails>();
+		studentList.add(new StudentDetails(1,"Ram","Kumar",123456789,"ram123@gmail.com",Date.valueOf("1994-01-22")));
+		Mockito.when(studentDao.findAll()).thenReturn(studentList);
+		List<StudentDetails> newStudentList=studentService.getAllStudentDetails();
+		Mockito.verify(studentDao,Mockito.times(1)).findAll();
 	}
 	
 	@Test
-	public void testAddStudentDetails() {
-		StudentDetails studentDetails = new StudentDetails();
-		studentDetails.setStudentId(1);
-		studentDetails.setFirstName("Raj");
-		studentDetails.setLastName("Patil");
-		studentDetails.setDateOfBirth(Date.valueOf("2000-09-03"));
-		studentDetails.setPhoneNumber(987654326l);
-		studentDetails.setStudentEmailId("raj03@gmail.com");
-		studentDetailsService.addStudentDetails(studentDetails);
-		//dao.save(studentDetails);
-		Assert.assertNotNull(studentDetails.getStudentId());
-		Mockito.verify(studentDetailsDao, Mockito.times(1)).save(studentDetails);
-	}
-	@Test
-	public void testGetAllStudentDetails() {
-		List<StudentDetails> studentDetailsList = new ArrayList<StudentDetails>();
-		studentDetailsList.add(new StudentDetails(1, "Raj", "Patil", 9875489624l, "raj02@gmail.com", Date.valueOf("2000-09-20")));
-		studentDetailsList.add(new StudentDetails(2, "Gopal", "Singh", 9856172534l, "gopalsingh@gmail.com", Date.valueOf("2001-03-30")));
-		
-		Mockito.when(studentDetailsDao.findAll()).thenReturn(studentDetailsList);
-		List<StudentDetails> list = studentDetailsService.getAllStudentDetails();
-		//Assertions.assertEquals(1, list.size());
-		Mockito.verify(studentDetailsDao, Mockito.times(1)).findAll();
-	}
-	@Test
-	public void testGetPaymentById() {
-		StudentDetails student=new StudentDetails(1,"Ajay","Kumar",987054321,"ajay1234@gmail.com",Date.valueOf("1995-01-01"));
-
-		Mockito.when(studentDetailsDao.getStudentDetailsById(1)).thenReturn(student);
-		StudentDetails newStudent=studentDetailsService.getStudentDetailsById(1);
-		//StudentDetails newStudent=paymentService.getPaymentById(1);
-		Mockito.verify(studentDetailsDao,Mockito.times(1)).getStudentDetailsById(1);
+	public void testGetStudentById() {
+		StudentDetails student=new StudentDetails(1,"Ram","Kumar",123456789,"ram123@gmail.com",Date.valueOf("1994-01-22"));
+		Mockito.when(studentDao.getStudentDetailsById(1)).thenReturn(student);
+		StudentDetails newStudent=studentService.getStudentDetailsById(1);
+		Mockito.verify(studentDao,Mockito.times(1)).getStudentDetailsById(1);
 	}
 	
+	@Test
+	public void testAddStudent() {
+		StudentDetails student=new StudentDetails(1,"Ram","Kumar",123456789,"ram123@gmail.com",Date.valueOf("1994-01-22"));
+		studentService.addStudentDetails(student);
+		Assert.assertNotNull(student.getStudentId());
+		Mockito.verify(studentDao,Mockito.times(1)).save(student);
+	}
 }
