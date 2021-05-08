5cc60a6cc21b6db42745636e661f85c7213399c0
Pavan R Shetty <pavanrshetty3@gmail.com>
2021-05-08 09:04:52 +0530

    comment added by Pavan

--- education-system/src/main/java/com/cg/educationsystem/service/ILoginService.java
+++ education-system/src/main/java/com/cg/educationsystem/service/ILoginService.java
@@ -5,5 +5,6 @@ import com.cg.educationsystem.entity.Login;
 public interface ILoginService {
 	//to validate user id and password 
 	public Login loginValidation(String userId,String userPassword,String role);
+	//to add new user credentials
 	public void addUser(Login login);
 }
